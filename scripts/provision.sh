c66cf9603c24f443d267537825be3cfd516c15f6
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2018-12-18 09:51:22 +0200

    update vagrant provision script

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
-# uopdate apt db
+# update apt db
 sudo apt-get update
 
 # install and configure go 1.10
 sudo apt-get install -y golang-1.10
 
+# configure go for root
 grep 'GOPATH|GOROOT' ~/.bash_profile &>/dev/null || {
   sudo mkdir -p ~/go
-  cp ~/.bash_profile ~/.bash_profile.ori 2>>/dev/null
-  grep -v 'GOPATH|GOROOT' ~/.bash_profile.ori 2>>/dev/null | sudo tee -a ~/.bash_profile
+  [ -f ~/.bash_profile ] && {
+    cp ~/.bash_profile ~/.bash_profile.ori
+    grep -v 'GOPATH|GOROOT' ~/.bash_profile.ori | sudo tee -a ~/.bash_profile
+  }
   echo 'export GOROOT=/usr/lib/go-1.10' | sudo tee -a ~/.bash_profile
   echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a ~/.bash_profile
   echo 'export GOPATH=~/go' | sudo tee -a ~/.bash_profile
   echo 'source ~/.bashrc' | sudo tee -a ~/.bash_profile
 }
 
+# configure go for vagrant user
 grep 'GOPATH|GOROOT' /home/vagrant/.bash_profile &>/dev/null || {
   sudo mkdir -p /home/vagrant/go
-  cp /home/vagrant/.bash_profile /home/vagrant/.bash_profile.ori 2>>/dev/null
-  grep -v 'GOPATH|GOROOT' /home/vagrant/.bash_profile.ori 2>>/dev/null | sudo tee -a /home/vagrant/.bash_profile
+  [ -f /home/vagrant/.bash_profile ] && {
+    cp /home/vagrant/.bash_profile /home/vagrant/.bash_profile.ori
+    grep -v 'GOPATH|GOROOT' /home/vagrant/.bash_profile.ori | sudo tee -a /home/vagrant/.bash_profile
+  }
   echo 'export GOROOT=/usr/lib/go-1.10' | sudo tee -a /home/vagrant/.bash_profile
   echo 'export PATH=$PATH:$GOROOT/bin' | sudo tee -a /home/vagrant/.bash_profile
   echo 'export GOPATH=/home/vagrant/go' | sudo tee -a /home/vagrant/.bash_profile
@@ -46,6 +52,9 @@ which git || {
   sudo apt-get install -y git
 }
 
+# install Kitchen-CI requirements
+sudo apt-get install -y ruby ruby-dev
+sudo gem install bundler
+
 # clean up
 sudo apt-get clean
-
